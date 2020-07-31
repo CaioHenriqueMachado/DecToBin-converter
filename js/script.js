f1e710b9ed51c0adda45a9c473f218286b6b5ce9
Caio <caio.student@outlook.com>
2020-07-31 18:49:08 -0300

    criando função binaria

--- js/script.js
+++ js/script.js
@@ -6,17 +6,32 @@ const octal = document.getElementById("oct");
 
 
 function fun(value) {
-  var decimal = document.getElementById("dec");
-  console.log(value);
-  // toBin(value);
+  toBin(value);
 
-  // console.log(value);
 };
 
-function toBin(value) {
-  
-  var binary = '';
-  var count = value;
-  count 
-}
+// function toBin(value) {
+//   var resul = value % 2;
+//   var count = value;
+//   while(rest != 1 || rest != 0){
+//     count/= 2;
+//   }
+//   var binary = '';
+//   var count = value;
+//   count 
+// }
 
+
+var bin = '';
+function binario (valor){
+  bin += valor % 2;
+
+  if (valor / 2 != 0 && valor / 2 != 1) {
+    binario(parseInt(valor/2 ,0));
+    
+  }else {
+    var resul = valor / 2;
+    resul += bin.split('').reverse().join('');
+    console.log(resul);
+  }
+}
