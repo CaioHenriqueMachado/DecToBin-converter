43808ca32514be15941caea78d15ff9531a142ad
Caio <caio.student@outlook.com>
2020-07-31 20:21:50 -0300

    inserindo a função hexadecimal

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ const octal = document.getElementById("oct");
 
 function fun(value) {
   toBin(value);
+  toHex(value);
 
 };
-
 
 var bin = '';
 function toBin(valor){
@@ -19,7 +19,7 @@ function toBin(valor){
     toBin(parseInt(valor/2 ,0));
     
   }else {
-    var result = valor / 2;
+    var result = parseInt(valor/2 ,0);
     result += bin.split('').reverse().join('');
     binary.value = result;
     bin = '';
@@ -27,4 +27,46 @@ function toBin(valor){
   }
 }
 
-toBin(16);
+var hex = '';
+function toHex(valor){
+  console.log(valor);
+
+  switch((valor % 16)){
+    case 10:
+      hex += "A";
+      break;
+    case 11:
+      hex += "B";
+      break;
+    case 12:
+      hex += "C";
+      break;
+    case 13:
+      hex += "D";
+      break;
+    case 14:
+      hex += "E";
+      break;
+    case 15:
+      hex += "F";
+      break;
+    default:
+      hex += valor % 16;
+      break;
+  }
+
+  if (valor / 16 > 15) {
+    toHex(parseInt(valor/16 ,0));
+    
+  }else {
+    var result = '';
+    if (parseInt(valor/16 ,0) > 0){
+      result = parseInt(valor/16 ,0);
+    }
+    
+    result += hex.split('').reverse().join('');
+    console.log(result);
+    hexadecimal.value = result;
+    hex = '';
+  }
+}
