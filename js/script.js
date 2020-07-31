87af50df601eb9b9533c93f1f14d8c055897043e
Caio <caio.student@outlook.com>
2020-07-31 17:55:10 -0300

    inserindo função

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+
+const hexadecimal = document.getElementById("hex");
+const binary = document.getElementById("bin");
+const octal = document.getElementById("oct");
+
+
+
 function fun(value) {
+  var decimal = document.getElementById("dec");
   console.log(value);
+  // toBin(value);
+
+  // console.log(value);
 };
+
+function toBin(value) {
+  
+  var binary = '';
+  var count = value;
+  count 
+}
+
